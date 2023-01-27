8da1b5b32f59687a9e7e8f1fa0fa09680c58cb8a
IndifferentSoul <whitecloudlurker@gmail.com>
2023-01-27 23:13:35 +0200

    Added a modal on party spot click

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,15 @@ import { builds } from './builds.js';
 
 const SQUADSIZE = 50;
 const squadContainer = document.querySelector('.squad-container');
+const modal = document.getElementById("myModal");
+const modalCloseX = document.querySelector(".close-modal");
+
+AddBuild('support');
+AddBuild('damage');
+AddBuild('celestial');
+AddBuild('other');
+
+makeGridBox();
 
 function makeGridBox() {
   let partySpot = 1;
@@ -10,31 +19,28 @@ function makeGridBox() {
   for (let i = 0; i < 50; i++) {
     if ((partySpot % 6) == 0) {
       partySpot = 1;
-      party++
+      party++;
     }
     const playerBox = document.createElement("div");
 
     playerBox.classList.add("player-box", "empty");
-    playerBox.id = `player-box-${party}-${partySpot}`
+    playerBox.id = `player-box-${party}-${partySpot}`;
 
     playerBox.ondragover = allowDrop;
     playerBox.ondrop = drop;
-    playerBox.draggable = true;
     playerBox.setAttribute('ondragstart', 'drag(event)')
+
+    playerBox.onclick = OpenModal;
 
     squadContainer.appendChild(playerBox);
 
-    partySpot++
+    partySpot++;
   };
 };
 
 function allowDrop(event) {
   event.preventDefault();
   event.dataTransfer.dropEffect = "move";
-};
-
-function noAllowDrop(event) {
-  event.stopPropagation();
 };
 
 function drop(event) {
@@ -50,27 +56,32 @@ function drop(event) {
   //move the player to the new spot or swap the two players
   if (draggedEle.classList.contains('build')) {
     const newDiv = MakePlayerContainer(draggedEle, targetSquare, draggedId);
-
     //If the spot is not empty replace the old one with the new build
     //else just append it to the spot
     if (targetSquare.hasChildNodes()) {
       const playerDiv = targetSquare.firstChild;
       targetSquare.replaceChild(newDiv, playerDiv);
     } else {
+      targetSquare.draggable = true;
+      
       targetSquare.appendChild(newDiv);
     };
   } else {
     const playerDiv = draggedEle.firstChild;
     if (targetSquare.classList.contains('empty')) {
       targetSquare.classList.remove('empty');
-      targetSquare.appendChild(playerDiv)
 
-      draggedEle.classList.add('empty')
+      targetSquare.draggable = true;
+      
+      targetSquare.appendChild(playerDiv);
+
+      draggedEle.classList.add('empty');
+      draggedEle.draggable = false;
     } else {
       const targetPlayer = targetSquare.firstChild;
 
       targetSquare.replaceChild(playerDiv, targetPlayer);
-      draggedEle.appendChild(targetPlayer)
+      draggedEle.appendChild(targetPlayer);
     };
   };
 };
@@ -78,10 +89,10 @@ function drop(event) {
 
 function MakePlayerContainer(draggedEle, targetSquare, draggedId) {
   const newDiv = document.createElement('div');
-  const playerProf = document.createElement('img')
+  const playerProf = document.createElement('img');
   const playerName = document.createElement('p');
 
-  targetSquare.classList.remove('empty')
+  targetSquare.classList.remove('empty');
 
   playerName.textContent = 'Name';
 
@@ -93,8 +104,8 @@ function MakePlayerContainer(draggedEle, targetSquare, draggedId) {
   newDiv.appendChild(playerProf);
   newDiv.appendChild(playerName);
 
-  return newDiv
-}
+  return newDiv;
+};
 
 
 function AddBuild(buildType) {
@@ -111,12 +122,22 @@ function AddBuild(buildType) {
     buildBox.setAttribute('ondragstart', 'drag(event)')
 
     boxInner.appendChild(buildBox);
-  }
-}
+  };
+};
 
-AddBuild('support');
-AddBuild('damage');
-AddBuild('celestial');
-AddBuild('other');
 
-makeGridBox();
+function OpenModal() {
+  modal.style.display = "block"
+};
+
+// When the user clicks on <span> (x), close the modal
+modalCloseX.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  };
+};
