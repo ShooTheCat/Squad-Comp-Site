258c3ab041fb50d749019c00f6607c5b23642a01
IndifferentSoul <whitecloudlurker@gmail.com>
2023-02-06 02:36:20 +0200

    Added boons to all builds

--- scripts/builds.js
+++ scripts/builds.js
@@ -5,7 +5,7 @@ export const builds = {
                 id: 'frb',
                 value: 1,
                 name: 'Support Firebrand',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vdA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
                 icon: './img/Specs/Firebrand.png',
                 boons: ['Aegis', 'Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Resolution', 'Stability', 'Swiftness', 'Vigor', 'Stunbreak']
             },
@@ -17,7 +17,7 @@ export const builds = {
                 name: 'Shoutbreaker',
                 link: 'http://en.gw2skills.net/editor/?PKwEwEmosAmEDj9xOxU6y+dB-zVJYjR3/ZkpUInVlqmmEcpBA-w',
                 icon: './img/Specs/Spellbreaker.png',
-                boons: ['Might', 'Quickness', 'Resistance', 'Stability', 'Swiftness', 'Vigor', 'Superspeed', 'Stunbreak']
+                boons: ['Might', 'Quickness', 'Resistance', 'Stability', 'Swiftness', 'Vigor', 'Stunbreak', 'Superspeed']
             },
         ],
         Revenant: [
@@ -27,7 +27,7 @@ export const builds = {
                 name: 'Heal Vindicator',
                 link: 'http://en.gw2skills.net/editor/?PmgAYZldQmMH6k1QpsHCl9RqsAqkF1zX0H-zVJYjR1fp0TSFUEkZoWdB47s0+mFA-w',
                 icon: './img/Specs/Vindicator.png',
-                boons: ['Might', 'Protection', 'Regeneration', 'Resistance', 'Swiftness', 'Stunbreak']
+                boons: ['Might', 'Protection', 'Regeneration', 'Resistance', 'Swiftness', 'Vigor', 'Stunbreak']
             },
         ],
         Engineer: [
@@ -37,7 +37,7 @@ export const builds = {
                 name: 'Heal Scrapper',
                 link: 'http://en.gw2skills.net/editor/?PewAo6JlNw8YPMM2JeySntUA-zVJYjRXfhEUUkZqUdB47s0+mFA-w',
                 icon: './img/Specs/Scrapper.png',
-                boons: ['Fury', 'Might', 'Protection', 'Regeneration', 'Swiftness', 'Vigor', 'Stealth', 'Stunbreak', 'Superspeed']
+                boons: ['Fury', 'Might', 'Protection', 'Regeneration', 'Stealth', 'Superspeed']
             },
         ],
         Ranger: [
@@ -45,7 +45,7 @@ export const builds = {
                 id: 'drd',
                 value: 1,
                 name: 'Heal Druid',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?POwEY6MssCGDbhFyKxyVSvlWy02D-zVJYjRNfhkUYkZCUdF47pIA7h3S7bWAA-w',
                 icon: './img/Specs/Druid.png',
                 boons: ['Fury', 'Protection', 'Regeneration', 'Stability', 'Vigor', 'Stunbreak']
             },
@@ -56,9 +56,9 @@ export const builds = {
                 id: 'chr',
                 value: 1,
                 name: 'Minstrel Chrono',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PiABw6x3lVw+YisMWJOuS1tWA-zVJYjx/ZkpUobVlGnGEcpBA-w',
                 icon: './img/Specs/Chronomancer.png',
-                boons: ['Fury', 'Protection', 'Regeneration', 'Stability', 'Vigor', 'Stunbreak']
+                boons: ['Aegis', 'Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Swiftness']
             },
         ],
         Elementalist: [
@@ -66,9 +66,9 @@ export const builds = {
                 id: 'tmp',
                 value: 1,
                 name: 'Support Tempest',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PGABg2hjlRwsYNMGWJOWTrtXA-zVJYjRffZkZKUdVo6s0+mFA-w',
                 icon: './img/Specs/Tempest.png',
-                boons: ['Aegis', 'Protection', 'Regeneration', 'Stability', 'Vigor', 'Stunbreak']
+                boons: ['Alacrity', 'Fury', 'Might', 'Protection', 'Regeneration', 'Swiftness', 'Vigor', 'Stunbreak', 'Superspeed']
             },
         ],
         Necromancer: [],
@@ -79,25 +79,25 @@ export const builds = {
                 id: 'drg',
                 value: 1,
                 name: 'DPS Dragonhunter',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PWwAMt/lFwaYOME2IO2L+tUA-zVJYiRNfh0UCkZIURFY7BIg9wbR/zIA-w',
                 icon: './img/Specs/Dragonhunter.png',
-                boons: ['Aegis', '', '', 'Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Resolution', 'Stability', 'Swiftness', 'Vigor']
+                boons: ['Aegis', 'Resolution', 'Vigor']
             },
             {
                 id: 'grd',
                 value: 2,
                 name: 'DPS Core Guardian',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PWwAYt3lFwmYYME2IO2L+raA-zVRYBR9IG+CppkIzQoiKw2DQA/to/ZEA-w',
                 icon: './img/Specs/Guardian.png',
-                boons: ['Aegis', '', '', 'Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Resolution', 'Stability', 'Swiftness', 'Vigor']
+                boons: ['Aegis', 'Might', 'Protection', 'Regeneration', 'Resolution', 'Stability', 'Swiftness']
             },
             {
                 id: 'wlb',
                 value: 3,
                 name: 'DPS Willbender',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PWwAEt/lFwwYIMV2IOyXlRLA-zVJYiR9fh0USVXIkZlayBIg/W0/MC-w',
                 icon: './img/Specs/Willbender.png',
-                boons: ['Aegis', '', '', 'Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Resolution', 'Stability', 'Swiftness', 'Vigor']
+                boons: ['Aegis', 'Alacrity', 'Might', 'Protection', 'Regeneration', 'Resolution', 'Stability', 'Swiftness']
             },
         ],
         Warrior: [
@@ -105,15 +105,17 @@ export const builds = {
                 id: 'brs',
                 value: 1,
                 name: 'Longbow Berserker',
-                link: 'http://en.gw2skills.net/editor/?PKwEwEmosAmEDj9xOxU6y+dB-zVJYjR3/ZkpUInVlqmmEcpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PKgAMFlRwCZgMKGJO+W6tKA-zVJYiR1fh0UimZIURVZ70IglNAA-w',
                 icon: './img/Specs/Berserker.png',
+                boons: ['Fury', 'Might', 'Swiftness']
             },
             {
                 id: 'spl',
                 value: 2,
                 name: 'DPS Spellbreaker',
-                link: 'http://en.gw2skills.net/editor/?PKwEwEmosAmEDj9xOxU6y+dB-zVJYjR3/ZkpUInVlqmmEcpBA-w',
+                link: 'http://gw2skills.net/editor/?PKgAEJlNwkYhsP2JO+W2vLA-zVJYiR1fpkZq2ZBURQ1dEY70IglNAA-w',
                 icon: './img/Specs/Spellbreaker.png',
+                boons: ['Resistance', 'Stability', 'Stunbreak', 'Superspeed']
             },
         ],
         Revenant: [
@@ -121,15 +123,17 @@ export const builds = {
                 id: 'hrl',
                 value: 1,
                 name: 'DPS Herald',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PmwAIxzlpQHMLyhdRNMM6hJSfsCKgJ/la7G-zVRYBh7TMqRglSTRQmhQEFQFJgtHgA2DvF9PjA-w',
                 icon: './img/Specs/Herald.png',
+                boons: ['Fury', 'Might', 'Protection', 'Regeneration', 'Stability', 'Swiftness', 'Superspeed']
             },
             {
                 id: 'vnd',
                 value: 2,
                 name: 'DPS Vindicator',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PmgAEJldQmMH6k1QpsHCl9RqsAqkJ/la0D-zVJYiR1fh0UBkZqWRUZ7BIgeL6fGBA-w',
                 icon: './img/Specs/Vindicator.png',
+                boons: ['Might', 'Regeneration', 'Resistance', 'Stability', 'Swiftness', 'Vigor', 'Stunbreak']
             },
         ],
         Engineer: [
@@ -137,15 +141,17 @@ export const builds = {
                 id: 'scr',
                 value: 1,
                 name: 'DPS Scrapper',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PegAIRlRw6YQsR2JO2LfteA-zVJYiRVfZkZqURVY7BIgeL6fGBA-w',
                 icon: './img/Specs/Scrapper.png',
+                boons: ['Protection', 'Stealth', 'Superspeed']
             },
             {
                 id: 'hls',
                 value: 2,
                 name: 'DPS Holosmith',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PewAQx7lVw6YRMJGKO2LfvXA-zVJYiRdfh0UEkZqURRY7BIgeL6fGBA-w',
                 icon: './img/Specs/Holosmith.png',
+                boons: ['Fury', 'Protection', 'Regeneration', 'Superspeed']
             },
         ],
         Ranger: [],
@@ -154,8 +160,9 @@ export const builds = {
                 id: 'drv',
                 value: 1,
                 name: 'DPS Daredevil',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PagAYZlNwwYbsOGJW2WbtaA-zVJYiR1fhkZg2ZVVRKY7BIg/W0/MCA-w',
                 icon: './img/Specs/Daredevil.png',
+                boons: ['Fury', 'Might', 'Swiftness', 'Stealth']
             },
         ],
         Mesmer: [],
@@ -164,15 +171,17 @@ export const builds = {
                 id: 'wvr',
                 value: 1,
                 name: 'DPS Weaver',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PGgAYZlFwoYesO2JO8KbvNA-zVJYiRVfh0UokZCURVY70ohlNAA-w',
                 icon: './img/Specs/Weaver.png',
+                boons: ['Fury', 'Protection', 'Regeneration', 'Stability','Swiftness']
             },
             {
                 id: 'ctl',
                 value: 2,
                 name: 'DPS Catalyst',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PGgAYZlpyoYesV2LO6KbxPA-zVJYiRVfh0UokZCURVY70ohlNAA-w',
                 icon: './img/Specs/Catalyst.png',
+                boons: ['Fury', 'Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Resolution', 'Swiftness', 'Vigor']
             },
         ],
         Necromancer: [
@@ -180,15 +189,17 @@ export const builds = {
                 id: 'scg',
                 value: 1,
                 name: 'DPS Scourge',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PSABsid3lNwQYHsHmJOqL6vcA-zVJYix/hkpUobRQOLAbNL6vsBA-w',
                 icon: './img/Specs/Scourge.png',
+                boons: ['Might', 'Stability', 'Swiftness']
             },
             {
                 id: 'rpr',
                 value: 2,
                 name: 'DPS Reaper',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PSwAEd3lRwQYHMMWJOqXnNbA-zVJYiR9/hkpUoCFQOjAbNL6vsBA-w',
                 icon: './img/Specs/Reaper.png',
+                boons: []
             },
         ],
     },
@@ -198,9 +209,9 @@ export const builds = {
                 id: 'frb',
                 value: 1,
                 name: 'Cele Firebrand',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PWABs2l7lRwuYYsOmJOcXbPVA-z1IY14/QqTFkugqDpgGnF6n2AA-w',
                 icon: './img/Specs/Firebrand.png',
-                boons: ['Aegis', '', '', 'Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Resolution', 'Stability', 'Swiftness', 'Vigor']
+                boons: ['Aegis', 'Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Resolution', 'Stability', 'Swiftness', 'Vigor', 'Stunbreak']
             },
         ],
         Warrior: [
@@ -208,8 +219,9 @@ export const builds = {
                 id: 'spl',
                 value: 1,
                 name: 'Cele Spellbreaker',
-                link: 'http://en.gw2skills.net/editor/?PKwEwEmosAmEDj9xOxU6y+dB-zVJYjR3/ZkpUInVlqmmEcpBA-w',
+                link: 'http://en.gw2skills.net/editor/?PKwEwEmosAmEDj9xOxU6y+dB-z1IY1o7/QyUC0tQInVlGnF6/1AA-w',
                 icon: './img/Specs/Spellbreaker.png',
+                boons: ['Might', 'Protection', 'Quickness', 'Regeneration', 'Resistance', 'Stability', 'Swiftness', 'Vigor', 'Stunbreak', 'Superspeed']
             },
         ],
         Revenant: [
@@ -217,8 +229,9 @@ export const builds = {
                 id: 'rng',
                 value: 1,
                 name: 'Cele Renegade',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
-                icon: './img/Specs/Revenant.png',
+                link: 'http://en.gw2skills.net/editor/?PmwAUlflhQHsIyhJRNMI6hBSfMBKgJzl1LH-zVRYVRNvYEMnSmC0KEUdrCyZKomZh+xNA-w',
+                icon: './img/Specs/Renegade.png',
+                boons: ['Alacrity', 'Fury', 'Might', 'Resistance', 'Stability', 'Swiftness']
             },
         ],
         Engineer: [],
@@ -229,8 +242,9 @@ export const builds = {
                 id: 'chr',
                 value: 1,
                 name: 'Cele Chrono',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PiABw2x7lNw+YNsMWJOuWptOA-z1IY14/M6WVlzUVjHiw83C9/aAA-w',
                 icon: './img/Specs/Chronomancer.png',
+                boons: ['Aegis', 'Alacrity', 'Might', 'Protection', 'Regeneration', 'Resistance', 'Swiftness']
             },
         ],
         Elementalist: [
@@ -238,8 +252,9 @@ export const builds = {
                 id: 'tmp',
                 value: 1,
                 name: 'Cele Tempest',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PGAFgsFaYZEsFWDziVij00b7F-z1IY1ov/QyUKEuIIdFgDnF6v0AA-w',
                 icon: './img/Specs/Tempest.png',
+                boons: ['Alacrity', 'Fury', 'Might', 'Protection', 'Regeneration']
             },
         ],
         Necromancer: [
@@ -247,8 +262,9 @@ export const builds = {
                 id: 'scg',
                 value: 1,
                 name: 'Cele Scourge',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PSABsid3lNwQYHsHmJOqL6vcA-z1IY14/M6WGkzSQjzi+LbAA-w',
                 icon: './img/Specs/Scourge.png',
+                boons: ['Might', 'Protection', 'Regeneration', 'Resistance', 'Stability', 'Swiftness']
             },
         ],
     },
@@ -262,15 +278,17 @@ export const builds = {
                 id: 'slb',
                 value: 1,
                 name: 'Immob beast',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?POwAExflRweYbMO2JOsK2PzSOWEA-zVJYiR9fhkZq2ZQVRFY70IglNAA-w',
                 icon: './img/Specs/Soulbeast.png',
+                boons: ['Fury', 'Stability']
             },
             {
                 id: 'unt',
                 value: 1,
                 name: 'CC Untamed',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?POgAIFlVwYZqMO2JWuPtRvSM/YA-zVJYiR1fhkZqWRE0dRZ7so/yGA-w',
                 icon: './img/Specs/Untamed.png',
+                boons: ['Fury', 'Regeneration']
             },
         ],
         Thief: [],
@@ -279,8 +297,9 @@ export const builds = {
                 id: 'vrt',
                 value: 1,
                 name: 'Strip Virtuoso',
-                link: 'http://en.gw2skills.net/editor/?PWwAYl7lRwkYYMOmJmyW7vbA-zVJYjR9/hkmCITpQ6iKNOL+fpBA-w',
+                link: 'http://gw2skills.net/editor/?PiwAEx3lVw2YtsPmLOuWpxKA-zVJYiR9/Z0tUNnVlamF9X2AA-w',
                 icon: './img/Specs/Virtuoso.png',
+                boons: ['Fury', 'Might', 'Swiftness']
             },
         ],
         Elementalist: [],
